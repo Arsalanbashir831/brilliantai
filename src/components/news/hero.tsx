8bc7bfbd63a3cb5669c0542b266549abe1763c84
Saadaan-Hassan <webmaker9d@gmail.com>
2025-06-18 18:37:41 +0500

    temp change in heading

--- src/components/news/hero.tsx
+++ src/components/news/hero.tsx
@@ -1,19 +1,23 @@
 // app/new/page.tsx
-'use client';
-
+"use client";
 
 export default function Hero() {
-    return (
-        <section className="relative w-full md:py-32 p-10  flex items-center justify-center bg-[url('/startup/hero.svg')] bg-cover bg-center text-white">
-            {/* dark overlay */}
-            <div className="absolute inset-0 bg-black/40"></div>
+	return (
+		<section className="relative w-full md:py-32 p-10  flex items-center justify-center bg-[url('/startup/hero.svg')] bg-cover bg-center text-white">
+			{/* dark overlay */}
+			<div className="absolute inset-0 bg-black/40"></div>
 
-            {/* content */}
-            <div className="relative z-10 max-w-4xl text-center md:px-4 ">
-                <h1 className="text-4xl md:text-7xl  font-md mb-4">News, insights and more</h1>
-                <p className="mb-6 text-lg text-center">Dive into our newsletter for expert insights, tips, and industry trends to elevate <br />your project management journey.</p>
-              
-            </div>
-        </section>
-    );
+			{/* content */}
+			<div className="relative z-10 max-w-4xl text-center md:px-4 ">
+				<h1 className="text-4xl md:text-7xl  font-md mb-4">
+					News, insights and more and more
+				</h1>
+				<p className="mb-6 text-lg text-center">
+					Dive into our newsletter for expert insights, tips, and industry
+					trends to elevate <br />
+					your project management journey.
+				</p>
+			</div>
+		</section>
+	);
 }
