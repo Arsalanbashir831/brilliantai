6de11f87fd23c87048e96bd218e0354edb9ce456
Saadaan-Hassan <webmaker9d@gmail.com>
2025-12-19 18:50:34 +0500

    Fix the following issue: "The "images.domains" configuration is deprecated. Please use "images.remotePatterns" configuration instead."

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,19 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 	/* config options here */
 	images: {
-		domains: [
-			"plus.unsplash.com",
-			"images.unsplash.com",
-			"storage.googleapis.com",
+		remotePatterns: [
+			{
+				hostname: "plus.unsplash.com",
+				protocol: "https",
+			},
+			{
+				hostname: "images.unsplash.com",
+				protocol: "https",
+			},
+			{
+				hostname: "storage.googleapis.com",
+				protocol: "https",
+			},
 		],
 	},
 };
