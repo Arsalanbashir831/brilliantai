659ce9aa9345c6fc60d8cee11c162bc643025bac
WahajHassan883 <wahajhassan007@gmail.com>
2025-12-22 14:40:58 +0500

    feat: Add MissionSection component to the about us page.

--- src/components/about-us/MissionSection.tsx
+++ src/components/about-us/MissionSection.tsx
@@ -7,7 +7,7 @@ import BrilliantButton from "../widgets/BrilliantButtons";
 export default function MissionSection() {
   return (
     <section className="py-20 ">
-      <div className="max-w-full mx-auto px-0  flex flex-col  items-center gap-4">
+      <div className="w-full mx-auto px-0 flex flex-col items-center gap-4">
         <h2 className="text-4xl md:text-[64px] text-center font-bold text-white">
           Our Mission
         </h2>
@@ -19,10 +19,10 @@ export default function MissionSection() {
           To turn ambitious ideas into high performing AI systems
         </p>
 
-        <div className="flex md:pl-32 px-5 flex-col md:flex-row items-center gap-12 mt-4 w-full text-center md:text-left text-[16px] text-normal pt-10">
+        <div className="flex md:pl-32 flex-col md:flex-row items-center gap-12 mt-4 w-full text-center md:text-left text-[16px] text-normal pt-10 md:pr-0">
           {/* — Left text block */}
-          <div className="md:w-1/2 space-y-6 text-white">
-            <div className="space-y-4">
+          <div className="flex-1 space-y-6 px-[50px] text-white max-w-xl">
+            <div className="space-y-10">
               <p
                 style={{
                   fontFamily: "Lufga Regular",
@@ -44,8 +44,8 @@ export default function MissionSection() {
                 style={{
                   fontFamily: "Lufga Regular",
                 }}>
-                We are the most cost-effective AI development partner in the UK,
-                delivering enterprise-level solutions with speed, precision, and
+                We are the most cost effective AI development partner in the UK,
+                delivering enterprise level solutions with speed, precision, and
                 focus without the unnecessary overhead.
               </p>
               <p
@@ -60,21 +60,19 @@ export default function MissionSection() {
             <BrilliantButton
               variant="white"
               className="mt-6 inline-block bg-white text-black font-medium px-2.5 md:px-6 py-3 rounded-xl hover:bg-gray-100 transition">
-              Let’s build what’s next
+              Let's build what is next
             </BrilliantButton>
           </div>
 
           {/* — Right image block */}
-          <div className="md:w-1/2 hidden md:block">
-            <div className="relative w-full rounded-l-2xl overflow-hidden shadow-xl">
+          <div className="flex-1 hidden md:block w-full">
+            <div className="relative w-full flex justify-end rounded-l-2xl overflow-hidden shadow-xl">
               {/* <ShineBorder shineColor={["#23D5D5", "#00FFFF"]} /> */}
               <Image
                 src="/about/mission.svg"
                 alt="Workflow of planning, coding, testing, building, monitoring"
-                width={0}
-                height={0}
-                sizes="100vw"
-                style={{ width: "100%", height: "auto" }}
+                width={672}
+                height={549}
                 className="object-contain"
               />
             </div>
